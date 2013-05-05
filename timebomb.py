4a926e6665c00a010e8b285f93bf535d6335a042
Andrew Frankel <andrew@leknarf.net>
2013-05-05 19:43:36 -0400

    boom

--- timebomb.py
+++ timebomb.py
@@ -1,13 +1,20 @@
 from flask import Flask
 import socket
 import time
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "{} - {}".format(
+    status_line = "{} - {}".format(
             socket.gethostname(), time.strftime("%X"))
+    return status_line
+
+@app.route('/boom', methods=['POST'])
+def boom():
+    os.system('killall gunicorn && rm -rf /opt/timebomb/')
+    return 'boom'
 
 if __name__ == '__main__':
     app.run()
